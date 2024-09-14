b90cfc90fb21ece7908395494aed799b9782836d
Paulo Guachizaca <yosnap@gmail.com>
2024-09-14 14:29:57 +0200

    Versión inicial

---
+++ bricks-api-integrator.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    * Plugin Name: Bricks API Integrator
+    * Description: Integra el constructor de páginas Bricks con APIs externas.
+    * Version: 1.0
+    * Author: Yosn4p Dev
+    * Author URI: https//yosn4p.dev
+    * License: GPL2
+    * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+    * Text Domain: bricks-api-integrator
+*/
